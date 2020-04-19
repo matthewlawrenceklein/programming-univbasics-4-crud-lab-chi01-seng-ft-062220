34d1f33cf9cb5b59dcc9b7251fd6adc03aa8c44d
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-19 18:54:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,29 @@
-def create_an_empty_array = []
-
+def create_an_empty_array
+    cats = []
 end
 
 def create_an_array
-
+    cats = ["milo", "bear", "sidney"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+    cats >> "benito"
 end
 
 def add_element_to_start_of_array(array, element)
-
+    cats.unshift("sushi")
 end
 
 def remove_element_from_end_of_array(array)
-
+    cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+    cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+    cats[2]
 end
 
 def retrieve_first_element_from_array(array)
